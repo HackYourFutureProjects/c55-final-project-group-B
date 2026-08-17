2e3d9e150bf654ca6b6056b6637a95ac250fa503
Dagim02 <dagimvanhaileselassie@gmail.com>
2026-08-17 14:18:37 +0200

    SCRUM-4: Add initial database schema migration for users table

--- backend/src/main/resources/db/migration/V1__init_schema.sql
+++ backend/src/main/resources/db/migration/V1__init_schema.sql
@@ -1,5 +1,8 @@
-create table users
-(
-    id   UUID not null constraint users_pk primary key,
-    email text NOT NULL
-);
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE IF NOT EXISTS users (
+                                     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+    );
