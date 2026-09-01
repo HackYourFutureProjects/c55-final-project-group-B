acaa37f369f14d68e7359b62114c9da9f66bb9f9
jivvy <jivvyjams@pm.me>
2026-09-01 20:07:24 +0200

    feat: styled success page after sign up

--- frontend/src/app/success/page.tsx
+++ frontend/src/app/success/page.tsx
@@ -1,8 +1,32 @@
+import { SiteFooter } from "@/components/site-footer";
+import { UserCircleCheckIcon } from "@phosphor-icons/react/ssr";
+import styles from "./page.module.css";
+
 export default function SuccessPage() {
   return (
-    <div className="container">
-      <p aria-hidden="true">🙂</p>
-      <h1>Success!</h1>
-    </div>
+    <>
+      <section className={styles.section}>
+        <div className="container">
+          <section className={styles.pane}>
+            <div className={styles.card}>
+              <UserCircleCheckIcon
+                size={64}
+                weight="duotone"
+                className={styles.icon}
+              />
+              <h1 className={styles.heading}>Welcome aboard!</h1>
+            </div>
+            <div className={styles.message}>
+              <p>
+                Your account has been created successfully. You can now save
+                jobs and come back to them anytime. Good luck!
+              </p>
+              <button className="button">Browse jobs</button>
+            </div>
+          </section>
+        </div>
+      </section>
+      <SiteFooter />
+    </>
   );
 }
