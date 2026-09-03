cec041403dc5e0313bb9403c4aee5d6940e8be08
jivvy <jivvyjams@pm.me>
2026-09-03 13:19:00 +0200

    feat: refactored success page component

--- frontend/src/app/success/page.tsx
+++ frontend/src/app/success/page.tsx
@@ -1,31 +1,24 @@
+import { StatusPage } from "@/components/status-page";
 import { UserCircleCheckIcon } from "@phosphor-icons/react/ssr";
 import Link from "next/link";
-import styles from "./page.module.css";
 
 export default function SuccessPage() {
   return (
-    <section className={styles.section}>
-      <div className="container">
-        <section className={styles.pane}>
-          <div className={styles.card}>
-            <UserCircleCheckIcon
-              size={64}
-              weight="duotone"
-              className={styles.icon}
-            />
-            <h1 className={styles.heading}>Welcome aboard!</h1>
-          </div>
-          <div className={styles.message}>
-            <p>
-              Your account has been created successfully. You can now save jobs
-              and come back to them anytime. Good luck!
-            </p>
-            <Link className="button" href={"/jobs"}>
-              Browse jobs
-            </Link>
-          </div>
-        </section>
-      </div>
-    </section>
+    <StatusPage
+      icon={UserCircleCheckIcon}
+      variant="success"
+      heading="Welcome aboard!"
+      children={
+        <p>
+          Your account has been created successfully. You can now save jobs and
+          come back to them anytime. Good luck!
+        </p>
+      }
+      actions={
+        <Link className="button" href={"/jobs"}>
+          Browse jobs
+        </Link>
+      }
+    />
   );
 }
